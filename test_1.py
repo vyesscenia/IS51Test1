d24b2103b3ff77fdb800f1acf845c3604afdaab0
Your Name <vyesscenia@mail.fresnostate.edu>
2021-04-15 22:54:46 -0700

    finished exam

--- test_1.py
+++ test_1.py
@@ -1,6 +1,5 @@
 
 
-
 """
 The program is trying to determin which payment is better (more money).
 Firtst option is 100 dollars per day for 10 days. The second option is 1 dollar
@@ -40,3 +39,33 @@ if the option2 is better, we output to the user "Option 2 is better"
 
 main
 """
+
+
+def option1():
+    return 100 * 10
+
+
+def option2():
+    amount = 1
+    list1 = []
+    for i in range(0, 10):
+        list1.append(amount)
+        amount *= 2
+    total = sum(list1)
+    return total
+
+
+def main():
+    answer = ""
+    var1 = option1()
+    var2 = option2()
+    if var1 == var2:
+        answer = "Option 1 and Option 2 pays the same."
+    elif var1 < var2:
+        answer = "Option 2 is better."
+    else:
+        answer = "Option 1 is better."
+    print(answer)
+
+
+main()
